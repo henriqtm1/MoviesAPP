cd134bf39784bbdbd8cfb2c83fbf7cb778adeb83
henriquet <henriquet@vegait.com>
2024-06-14 09:24:42 -0300

    add: formated build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         getByName("main").java.srcDirs("build/generated/source/navigation-args")
     }
 
-    buildFeatures{
+    buildFeatures {
         buildConfig = true
         viewBinding = true
     }
@@ -41,7 +41,8 @@ android {
             buildConfigField(
                 "String",
                 "API_MOVIES",
-                "\"https://api.themoviedb.org\"")
+                "\"https://api.themoviedb.org\""
+            )
 
             signingConfig = signingConfigs.getByName("debug")
         }
@@ -55,7 +56,8 @@ android {
             buildConfigField(
                 "String",
                 "API_MOVIES",
-                "\"https://api.themoviedb.org\"")
+                "\"https://api.themoviedb.org\""
+            )
 
             applicationIdSuffix = ".debug"
             isMinifyEnabled = false
@@ -77,22 +79,22 @@ dependencies {
     var koin_version = "3.1.2"
 
     // JUnit
-    testImplementation ("junit:junit:4.13.2")
+    testImplementation("junit:junit:4.13.2")
     // Mockito
-    testImplementation ("org.mockito:mockito-core:$mockito_version")
-    testImplementation ("org.mockito:mockito-inline:$mockito_version")
-    testImplementation ("org.mockito.kotlin:mockito-kotlin:$mockito_version")
+    testImplementation("org.mockito:mockito-core:$mockito_version")
+    testImplementation("org.mockito:mockito-inline:$mockito_version")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:$mockito_version")
     // webmock
-    testImplementation ("com.squareup.okhttp3:mockwebserver:4.9.3")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.9.3")
     // FragmentTesting
-    debugImplementation ("androidx.fragment:fragment-testing:1.8.0")
+    debugImplementation("androidx.fragment:fragment-testing:1.8.0")
     // Coroutines Test
-    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     // AndroidX Test
-    testImplementation ("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
     // Koin for Testing
     testImplementation("io.insert-koin:koin-test:$koin_version")
-    androidTestImplementation ("io.insert-koin:koin-test:$koin_version")
+    androidTestImplementation("io.insert-koin:koin-test:$koin_version")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     // Navigation
@@ -107,27 +109,27 @@ dependencies {
     // Material
     implementation(libs.material)
     // Lifecycles only (without ViewModel or LiveData)
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
     // ViewModel
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.2")
     // LiveData
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.2")
     // Coroutines
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     // okhttp
-    implementation ("com.google.code.gson:gson:2.10.1")
-    implementation ("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
     // retrofit
-    implementation ("com.squareup.retrofit2:retrofit:$retrofit_version")
-    implementation ("com.squareup.retrofit2:converter-gson:$retrofit_version")
-    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.4.0")
-    implementation ("com.squareup.retrofit2:converter-scalars:2.3.0")
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:2.4.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.3.0")
     // kotlin based dependency injection koin
-    implementation ("io.insert-koin:koin-core:$koin_version")
-    implementation ("io.insert-koin:koin-android:$koin_version")
+    implementation("io.insert-koin:koin-core:$koin_version")
+    implementation("io.insert-koin:koin-android:$koin_version")
     // curl
-    implementation ("com.github.mrmike:ok2curl:0.8.0")
+    implementation("com.github.mrmike:ok2curl:0.8.0")
 
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
 }
